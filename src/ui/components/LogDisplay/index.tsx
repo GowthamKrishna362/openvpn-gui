9311aad36a9dd61ed1e46bc770790dfce66b4989
Gowtham <gks0321@gmail.com>
2025-06-14 11:06:38 +0530

    Add logs header

--- src/ui/components/LogDisplay/index.tsx
+++ src/ui/components/LogDisplay/index.tsx
@@ -3,5 +3,10 @@ import "./logDisplay.css";
 type LogDispayProps = { logs: string | null };
 
 export default function LogDisplay({ logs }: LogDispayProps) {
-  return <div className="log-display">{logs}</div>;
+  return (
+    <div className="log-display-container">
+      <div className="log-display-header"> Logs </div>
+      <div className="log-display">{logs}</div>
+    </div>
+  );
 }
